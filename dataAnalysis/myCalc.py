f0604e84827cb51d2ac4e46a74ceafa04223c9a9
ock-kyung2 <dhrwlsrud94@naver.com>
2024-10-29 21:11:39 +0900

    dataAnalysis 강의 실습

---
+++ dataAnalysis/myCalc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def get_plus(a,b):
+    return a+b
+def get_minus(a,b):
+    return a-b
+def get_multiply(a,b):
+    return a*b
+def get_division(a,b):
+    if b!=0:
+        return a/b
+    else :
+        'zero division error'
+
