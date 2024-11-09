66c73911d6495ef6b6ed97ed02956dbf26fd76be
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-09 12:23:18 +0900

    todoList map 메소드로 나열하기

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -2,15 +2,51 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  btnStyle = {
+    padding: "5px 9px",
+    color: "#fff",
+    border: "none",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float : "right"
+  }
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
+
+  todoList = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false
+    },
+  ];
   render() {
     return (
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
+      <div className='container'>
+        <div className='todoBlock'>
+          <div className='title'>
             <h1>할 일 목록</h1>
           </div>
+          {this.todoList.map((list) => (
+            <div style={this.getStyle()} key={list.id}>
+              <input type='checkbox' defaultChecked={list.completed}/>
+              {list.title}
+              <button style={this.btnStyle}>X</button>
+            </div>
+          ))}
         </div>
       </div>
-    )
+    );
   }
 }
