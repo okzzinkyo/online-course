d19e18b715727f33cd30f05aab4f1c79ad3301de
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-09 12:50:18 +0900

    react state 사용해서 변경된 todolist 브라우저에 랜더링 하기

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  state = {
+    todoList : [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false
+      },
+    ],
+    value: "",
+  }
+
   btnStyle = {
     padding: "5px 9px",
     color: "#fff",
@@ -19,22 +35,9 @@ export default class App extends Component {
     }
   }
 
-  todoList = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    },
-  ];
-
   handleClick = (id) => {
-    let newTodoList = this.todoList.filter((list) => list.id !== id);
-    console.log('newTodoList: ', newTodoList);
+    let newTodoList = this.state.todoList.filter((list) => list.id !== id);
+    this.setState({ todoList: newTodoList });
   }
 
   render() {
@@ -44,7 +47,7 @@ export default class App extends Component {
           <div className='title'>
             <h1>할 일 목록</h1>
           </div>
-          {this.todoList.map((list) => (
+          {this.state.todoList.map((list) => (
             // key 속성
             // 1,2 뒤에 3리스트를 넣을 때는 3번째 리스트만 추가하면 된다고 인식하지만, 3리스트를 앞에 넣을 때는 1,2 모두 변경됐다고 인식하기 때문에 모든 자식 엘리먼트를 새롭게 그려버린다.
             // 따라서 key 속성을 유니크한 값으로 부여하면 어떤 부분이 바뀌었는지 정확하게 인식할 수 있다.
