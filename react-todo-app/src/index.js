23cd21aff5c3934e38dccb0dd43eab9483da8331
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-20 00:41:54 +0900

    react-beautiful-dnd 라이브러리로 드래그 앤 드롭 기능 추가하기

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -5,9 +5,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // React 18 버전에서 드래그 앤 드랍 사용 시 발생하는 에러로 인한 <React.StrictMode></React.StrictMode> 제거
+  // Invariant failed: Cannot find droppable entry with id
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
