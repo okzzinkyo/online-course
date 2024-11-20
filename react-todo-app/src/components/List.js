baf99d0369f154df35cc384c4fb1bc2a897e2bbb
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-20 16:24:20 +0900

    todo list 수정하기 기능 추가

--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 // React.memo를 적용하기 위해 arrow function으로 변경
 
 const List = React.memo(({ id, title, completed, todoList, setTodoList, provided, snapshot, handleClick }) => {
-  console.log('List component');
+  // console.log('List component');
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
 
   const handleCompleteChange = (id) => {
     let newTodoList = todoList.map((list) => {
@@ -16,33 +19,75 @@ const List = React.memo(({ id, title, completed, todoList, setTodoList, provided
     setTodoList(newTodoList);
   };
 
-  return (
-    <div
-      key={id}
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${
-        snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'
-      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-500 bg-gray-100 border rounded`}
-    >
-      <div className='items-center'>
-        <input
-          type='checkbox'
-          onChange={() => {
-            handleCompleteChange(id);
-          }}
-          defaultChecked={completed}
-        />{' '}
-        <span className={completed ? 'line-through' : undefined}>{title}</span>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodoList = todoList.map((list) => {
+      if (list.id === id) {
+        list.title = editedTitle;
+      }
+      return list;
+    });
+
+    setTodoList(newTodoList);
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <div className={`flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}>
+        <div className='items-center'>
+          <form onSubmit={handleSubmit}>
+            <input
+              value={editedTitle}
+              className='w-full px-3 py-2 mr-4 text-gray-500 rounded'
+              onChange={(e) => setEditedTitle(e.target.value)}
+              autoFocus
+            />
+          </form>
+        </div>
+        <div className='items-center'>
+          <button type='button' className='float-rignt px-4 py-2' onClick={handleSubmit}>
+            save
+          </button>
+          <button type='submit' className='px-4 py-2' onClick={() => setIsEditing(false)}>
+            X
+          </button>
+        </div>
       </div>
-      <div className='items-center'>
-        <button className='px-4 py-2' onClick={() => handleClick(id)}>
-          X
-        </button>
+    );
+  } else {
+    return (
+      <div
+        key={id}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${
+          snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'
+        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-500 bg-gray-100 border rounded`}
+      >
+        <div className='items-center'>
+          <input
+            type='checkbox'
+            onChange={() => {
+              handleCompleteChange(id);
+            }}
+            defaultChecked={completed}
+          />{' '}
+          <span className={completed ? 'line-through' : undefined}>{title}</span>
+        </div>
+        <div className='items-center'>
+          <button className='px-4 py-2' onClick={() => setIsEditing((prev) => !prev)}>
+            edit
+          </button>
+          <button className='px-4 py-2' onClick={() => handleClick(id)}>
+            X
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 });
 
 export default List;
