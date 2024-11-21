f7ce7a6d4c570a2e516c0f02aca0132cc05d67ad
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-21 21:02:16 +0900

    테마별 영화 나열을 위한 row section 추가

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,12 +1,19 @@
 import Banner from 'components/Banner';
 import './App.css';
 import Nav from './components/Nav';
+import Row from 'components/Row';
+import requests from 'api/requests';
 
 function App() {
   return (
     <div className='app'>
       <Nav></Nav>
       <Banner></Banner>
+      <Row title='NETFLEX ORIGINALS' id='NO' fetchUrl={requests.fetchNetflixOriginals} isLargeRow></Row>
+      <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} isLargeRow></Row>
+      <Row title='TOP Rated' id='TR' fetchUrl={requests.fetchTopRated} isLargeRow></Row>
+      <Row title='Action Movies' id='AM' fetchUrl={requests.fetchActionMovies} isLargeRow></Row>
+      <Row title='Comedy Movies' id='CM' fetchUrl={requests.fetchComedyMovies} isLargeRow></Row>
     </div>
   );
 }
