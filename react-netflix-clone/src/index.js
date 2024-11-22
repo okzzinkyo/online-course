93cd5b833e37c2e0c90818f3d96a0cbc9d091188
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-22 23:48:53 +0900

    gh-pages 경로 수정

--- react-netflix-clone/src/index.js
+++ react-netflix-clone/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='online-course/react-netflix-clone'>
+  <BrowserRouter basename='online-course'>
     <App />
   </BrowserRouter>
 );
