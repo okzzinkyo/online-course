42217b5972d2557dff714963433df7a33513c79e
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-22 22:29:25 +0900

    gitpage 배포 경로 수정

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='online-course'>
+  <BrowserRouter basename='online-course/react-netflix-clone'>
     <App />
   </BrowserRouter>
 );
