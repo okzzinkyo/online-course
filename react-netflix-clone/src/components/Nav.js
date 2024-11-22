76b448e9cd8daa4f5e464ad8366bcb262c08bd2a
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-22 20:18:31 +0900

    nav className 오타 수정 및 코드 정리

--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './Nav.css';
 import profile from 'assets/images/user-profile.jpg';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useNavigation } from 'react-router-dom';
 
 export default function Nav() {
+  const navigate = useNavigate();
+
   const [show, setShow] = useState(false);
   const [searchValue, setSearchValue] = useState('');
-  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -33,10 +34,10 @@ export default function Nav() {
         className='nav__logo'
         src={'https://i.namu.wiki/i/MItAcktgp0wbRDTzFmack6FrbMbFJ_4qFw8opZnTFwXDbl1SS4r7IXwy32J2_IcfVEs_xlJcaFLFuPZDHLU-Uw.svg'}
         alt='Netflix logo'
-        onClick={() => window.location.reload()}
+        onClick={() => navigate('/')}
       />
       <input className='nav__input' type='text' value={searchValue} onChange={handleChange} placeholder='영화를 검색해 주세요.'></input>
-      <img className='nav__avator' src={profile} alt='User_logged'></img>
+      <img className='nav__avatar' src={profile} alt='User_logged'></img>
     </nav>
   );
 }
