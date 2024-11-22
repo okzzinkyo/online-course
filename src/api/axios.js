05478be73ce564d2461f94a6cdf06b8a2c603051
ock-kyung2 <dhrwlsrud94@naver.com>
2024-11-22 21:26:04 +0900

    api 관련 환경 변수 적용

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
-    api_key: 'a265076a78d9f4b6adef8b080a4e7520',
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: 'ko-KR',
   },
 });
